e9751c01a3fd9b70fd9c5dd2102959130862f5cb
Nikk0PT <61123353+Nikk0PT@users.noreply.github.com>
2020-04-23 10:05:01 +0100

    Add files via upload

---
+++ MDI Login/MDI Login/Login.vb
@@ -0,0 +1,9 @@
+﻿Public Class Login
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If TextBox1.Text = "admin" And TextBox2.Text = "admin" Then
+            MsgBox("Bem Vindo!")
+        Else
+            MsgBox("Tente Outra Vez!")
+        End If
+    End Sub
+End Class
